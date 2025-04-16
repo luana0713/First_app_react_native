dbc08b93e971c887d847376a0e3957c76dca3a5d
luana0713 <166567157+luana0713@users.noreply.github.com>
2025-04-16 13:45:48 -0300

    new update

--- my-project-js/App.js
+++ my-project-js/App.js
@@ -1,15 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello my future husband</Text>
+      <Text>Army app</Text>
       <StatusBar style="auto" />
+      <Text>Army app</Text>
     </View>
+    //<View>
+     //   <Button title="hi" onPress={() => alert('pr!')} />
+     // </View>
+  //  </View>
+ 
+    
+
   );
 }
 
+
+      //eu tenho o View e dentro desse view eu uso uma Text //
 const styles = StyleSheet.create({
   container: {
     flex: 1,
